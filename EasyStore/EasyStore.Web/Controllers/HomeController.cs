bc32444b9c53a9aa0a334936f6990b73d60266e6
Thiago Cruz <thagocruz@gmail.com>
2021-12-20 15:12:03 -0300

    Add 3.1 version

--- EasyStore/EasyStore.Web/Controllers/HomeController.cs
+++ EasyStore/EasyStore.Web/Controllers/HomeController.cs
@@ -1,30 +1,37 @@
 ﻿using EasyStore.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace EasyStore.Web.Controllers;
-public class HomeController : Controller
+namespace EasyStore.Web.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
--- EasyStore/EasyStore.Web/Models/ErrorViewModel.cs
+++ EasyStore/EasyStore.Web/Models/ErrorViewModel.cs
@@ -1,8 +1,11 @@
+using System;
 
-namespace EasyStore.Web.Models;
-public class ErrorViewModel
+namespace EasyStore.Web.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
