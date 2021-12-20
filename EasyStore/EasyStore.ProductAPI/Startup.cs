18c923b3e00078dd347b7cbf29d59cfa357b797d
Thiago Cruz <thagocruz@gmail.com>
2021-12-20 15:51:48 -0300

    Add base connection string

--- EasyStore/EasyStore.ProductAPI/Startup.cs
+++ EasyStore/EasyStore.ProductAPI/Startup.cs
@@ -1,7 +1,9 @@
+using EasyStore.ProductAPI.DbContexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,7 @@ namespace EasyStore.ProductAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
 
