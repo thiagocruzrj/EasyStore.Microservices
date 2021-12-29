cf8cdf9510d5e5ddbd169131b6ca54247f934908
Thiago Cruz <thagocruz@gmail.com>
2021-12-29 17:03:20 -0300

    Create ProductService.cs

---
+++ EasyStore/EasyStore.Web/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using EasyStore.Web.Models;
+using EasyStore.Web.Services.IServices;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EasyStore.Web.Services
+{
+    public class ProductService : BaseService, IProductService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public ProductService(IHttpClientFactory httpClient) : base(httpClient)
+        {
+            _clientFactory = httpClient;
+        }
+
+        public Task<T> CreateProductByIdAsync<T>(ProductDto productDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteProductByIdAsync<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAllProductsAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetProductByIdAsync<T>(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateProductByIdAsync<T>(ProductDto productDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
