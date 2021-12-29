f3baee1fa43c882bf986dcef652d8eb41b68ec22
Thiago Cruz <thagocruz@gmail.com>
2021-12-29 16:35:07 -0300

    Update BaseService.cs

--- EasyStore/EasyStore.Web/Services/IServices/BaseService.cs
+++ EasyStore/EasyStore.Web/Services/IServices/BaseService.cs
@@ -1,6 +1,8 @@
 ﻿using EasyStore.Web.Models;
+using Newtonsoft.Json;
 using System;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace EasyStore.Web.Services.IServices
@@ -18,7 +20,41 @@ namespace EasyStore.Web.Services.IServices
 
         public Task<T> SendAsync<T>(ApiRequest apiRequest)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var client = HttpClient.CreateClient("EasyStoreApi");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+                client.DefaultRequestHeaders.Clear();
+
+                if(apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data), Encoding.UTF8, "application/json");
+                }
+
+                HttpRequestMessage apiResponse = null;
+                switch (apiRequest.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public void Dispose()
