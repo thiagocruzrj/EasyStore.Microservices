bd1fdc01614fe412faebb9c2f2fd23148adca27f
Thiago Cruz <thagocruz@gmail.com>
2021-12-29 13:42:44 -0300

    Create ProductApiController.cs

---
+++ EasyStore/EasyStore.ProductAPI/Controllers/ProductApiController.cs
@@ -0,0 +1,19 @@
+﻿using EasyStore.ProductAPI.Models.Dto;
+using EasyStore.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyStore.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    public class ProductApiController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IProductRepository _repository;
+
+        public ProductApiController(IProductRepository repository)
+        {
+            _repository = repository;
+            _response = new ResponseDto();
+        }
+    }
+}
