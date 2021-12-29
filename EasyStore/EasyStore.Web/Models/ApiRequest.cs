7eb222b2031f3490354777a657abba28812fefaa
Thiago Cruz <thagocruz@gmail.com>
2021-12-29 16:19:23 -0300

    Create ApiRequest.cs

---
+++ EasyStore/EasyStore.Web/Models/ApiRequest.cs
@@ -0,0 +1,12 @@
+﻿using static EasyStore.Web.SD;
+
+namespace EasyStore.Web.Models
+{
+    public class ApiRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
