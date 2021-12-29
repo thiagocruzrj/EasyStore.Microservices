b6f268695e7efb6d41bed808cf31754e88e4db11
Thiago Cruz <thagocruz@gmail.com>
2021-12-29 15:51:12 -0300

    Seeding products

---
+++ EasyStore/EasyStore.ProductAPI/Scripts/SeedProducts.sql
@@ -0,0 +1,45 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Products] (
+    [ProductId] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Price] float NOT NULL,
+    [Description] nvarchar(max) NULL,
+    [CategoryName] nvarchar(max) NULL,
+    [ImageUrl] nvarchar(max) NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY ([ProductId])
+);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20211229155306_AddProductModelToDb', N'3.1.22');
+
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'ProductId', N'CategoryName', N'Description', N'ImageUrl', N'Name', N'Price') AND [object_id] = OBJECT_ID(N'[Products]'))
+    SET IDENTITY_INSERT [Products] ON;
+INSERT INTO [Products] ([ProductId], [CategoryName], [Description], [ImageUrl], [Name], [Price])
+VALUES (1, N'Appetizer', N'Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.', N'https://dotnetmastery.blob.core.windows.net/mango/14.jpg', N'Samosa', 15.0E0),
+(2, N'Appetizer', N'Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.', N'https://dotnetmastery.blob.core.windows.net/mango/12.jpg', N'Paneer Tikka', 13.99E0),
+(3, N'Dessert', N'Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.', N'https://dotnetmastery.blob.core.windows.net/mango/11.jpg', N'Sweet Pie', 10.99E0),
+(4, N'Entree', N'Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.', N'https://dotnetmastery.blob.core.windows.net/mango/13.jpg', N'Pav Bhaji', 15.0E0);
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'ProductId', N'CategoryName', N'Description', N'ImageUrl', N'Name', N'Price') AND [object_id] = OBJECT_ID(N'[Products]'))
+    SET IDENTITY_INSERT [Products] OFF;
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20211229185012_SeedProducts', N'3.1.22');
+
+GO
+
