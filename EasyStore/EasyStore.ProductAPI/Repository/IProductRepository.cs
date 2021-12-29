66b006ae4a7b52da7bdfec2fa898241f057f0787
Thiago Cruz <thagocruz@gmail.com>
2021-12-29 13:07:01 -0300

    Add base methods

--- EasyStore/EasyStore.ProductAPI/Repository/IProductRepository.cs
+++ EasyStore/EasyStore.ProductAPI/Repository/IProductRepository.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using EasyStore.ProductAPI.Models.Dto;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace EasyStore.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> UpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
     }
 }
