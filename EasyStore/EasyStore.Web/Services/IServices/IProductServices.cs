3e0dc16943ca1bd31b999d98505accdca5eacb0f
Thiago Cruz <thagocruz@gmail.com>
2021-12-29 16:09:53 -0300

    Create IProductServices.cs

---
+++ EasyStore/EasyStore.Web/Services/IServices/IProductServices.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace EasyStore.Web.Services.IServices
+{
+    public interface IProductServices
+    {
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductByIdAsync<T>(int Id);
+        Task<T> CreateProductByIdAsync<T>();
+        Task<T> UpdateProductByIdAsync<T>();
+        Task<T> DeleteProductByIdAsync<T>(int id);
+    }
+}
