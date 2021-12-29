b02b073b5777af52a6fd6fe74c152d7d663fe3d9
Thiago Cruz <thagocruz@gmail.com>
2021-12-29 16:28:32 -0300

    Create BaseService.cs

---
+++ EasyStore/EasyStore.Web/Services/IServices/BaseService.cs
@@ -0,0 +1,29 @@
+﻿using EasyStore.Web.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EasyStore.Web.Services.IServices
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDto ResponseModel { get ; set; }
+        public IHttpClientFactory HttpClient { get; set; }
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            ResponseModel = new ResponseDto();
+            HttpClient = httpClient;
+        }
+
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(true);
+        }
+    }
+}
