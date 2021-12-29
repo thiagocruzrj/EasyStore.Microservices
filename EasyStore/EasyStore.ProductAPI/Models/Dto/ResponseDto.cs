4c9b982a19a84e7e62533def56f200c8d19af901
Thiago Cruz <thagocruz@gmail.com>
2021-12-29 13:04:51 -0300

    Update ResponseDto.cs

--- EasyStore/EasyStore.ProductAPI/Models/Dto/ResponseDto.cs
+++ EasyStore/EasyStore.ProductAPI/Models/Dto/ResponseDto.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace EasyStore.ProductAPI.Models.Dto
 {
     public class ResponseDto
     {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = "";
+        public List<string> ErrorMessages { get; set; }
     }
 }
