5f4fa668aa9bc1c22ef4351d551c4fd84378a430
Thiago Cruz <thagocruz@gmail.com>
2021-12-29 13:19:35 -0300

    Create ProductRepository.cs

---
+++ EasyStore/EasyStore.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using EasyStore.ProductAPI.DbContexts;
+using EasyStore.ProductAPI.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EasyStore.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private IMapper _mapper;
+
+        public ProductRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public Task<ProductDto> GetProductById(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductDto> UpdateProduct(ProductDto productDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
