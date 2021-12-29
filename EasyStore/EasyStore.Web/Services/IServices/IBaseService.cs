edfe92fd7724816f99beec325a9c4144cf26a285
Thiago Cruz <thagocruz@gmail.com>
2021-12-29 16:20:50 -0300

    Create IBaseService.cs

---
+++ EasyStore/EasyStore.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿using EasyStore.Web.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace EasyStore.Web.Services.IServices
+{
+    public interface IBaseService : IDisposable
+    {
+        ResponseDto ResponseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
