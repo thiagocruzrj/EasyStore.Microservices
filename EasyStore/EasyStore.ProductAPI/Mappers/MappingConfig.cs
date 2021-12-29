3ed299f62fd0db77587c1107fbed8af0cef715f5
Thiago Cruz <thagocruz@gmail.com>
2021-12-29 13:10:09 -0300

    Create MappingConfig.cs

---
+++ EasyStore/EasyStore.ProductAPI/Mappers/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using EasyStore.ProductAPI.Models;
+using EasyStore.ProductAPI.Models.Dto;
+
+namespace EasyStore.ProductAPI.Mappers
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(c =>
+            {
+                c.CreateMap<ProductDto, Product>();
+                c.CreateMap<Product, ProductDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
